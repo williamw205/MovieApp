efea8eb44723413d168eebac8088b08330639e12
William Wong <williamw205@gmail.com>
2023-11-16 19:40:03 -0600

    Initial Commit

---
+++ MovieFinderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieFinderApp
+//
+//  Created by William Wong on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieFinderApp/MovieFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderAppApp.swift
+//  MovieFinderApp
+//
+//  Created by William Wong on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
