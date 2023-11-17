d2a2f78b694fbfacb22bb193a9dfbdf469ff9b1e
William Wong <williamw205@gmail.com>
2023-11-17 00:33:55 -0600

    implementing images within app

---
+++ MovieFinderApp/MovieInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieInfoView.swift
+//  MovieFinderApp
+//
+//  Created by William Wong on 11/17/23.
+//
+
+import SwiftUI
+
+struct MovieInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieInfoView()
+}
